7585fd727ac7d201f6f85edc1bdbcdb9bb75ccfb
ypiard85 <60528207+ypiard85@users.noreply.github.com>
2021-08-23 11:56:04 +0200

    models

---
+++ app/Models/Promo_articles.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Promo_articles extends Model
+{
+    use HasFactory;
+}
